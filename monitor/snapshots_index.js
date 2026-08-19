92789246fb1fd30ac096bb1f9fb0063df4d3bc31
hawchou1995 <hawchou1995@users.noreply.github.com>
2026-08-20 07:33:15 +0800

    deploy: 2026-08-20 07:33

--- monitor/snapshots_index.js
+++ monitor/snapshots_index.js
@@ -1,1 +1,1 @@
-window.SNAPSHOTS = {"snapshots": [{"date": "2026-08-17", "file": "20260817_dual.html", "count": "40+24", "dual": true}], "months": [{"month": "2026-08", "items": [{"date": "2026-08-17", "file": "20260817_dual.html", "count": "40+24", "dual": true}]}]};
+window.SNAPSHOTS = {"snapshots": [{"date": "2026-08-19", "file": "20260819_dual.html", "count": "33+17", "dual": true}], "months": [{"month": "2026-08", "items": [{"date": "2026-08-19", "file": "20260819_dual.html", "count": "33+17", "dual": true}]}]};
